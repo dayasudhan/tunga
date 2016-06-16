b4900fd0a9a7218cc422440a4eb85130c548d1f7
dganeshappa <dganeshappa@paypal.com>
2016-06-17 00:37:40 +0530

    order url fix

--- app/src/main/java/materialapp/survivingwithandroid/com/materialapp/Utils/Constants.java
+++ app/src/main/java/materialapp/survivingwithandroid/com/materialapp/Utils/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
     public static final String RELEASE_URL = "http://oota.herokuapp.com";
     public static final String DEBUG_URL = "http://kuruva.herokuapp.com";
     public static final String MAIN_URL = DEBUG_URL;
-    public static final String ORDER_URL = MAIN_URL + "/v1/vendor/order";
+    public static final String ORDER_URL = MAIN_URL + "/v1/vendor/order/";
     public static final String LOGIN_URL =  MAIN_URL +"/v1/m/login";
     public static final String GET_MENU = MAIN_URL +"/v1/vendor/menu/";
     public static final String DELETE_MENU= MAIN_URL + "/v1/vendor/menu/item/";
